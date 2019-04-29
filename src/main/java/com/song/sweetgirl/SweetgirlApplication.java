ea0b2b9782e3b9db600ddaf1d9c7d11621ca98ea
songlw <songlw@yunwutech.com>
2019-04-29 15:11:11 +0800

    first commit

---
+++ src/main/java/com/song/sweetgirl/SweetgirlApplication.java
@@ -0,0 +1,13 @@
+package com.song.sweetgirl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetgirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetgirlApplication.class, args);
+	}
+
+}
