be5ec7c4045ff146ffcd7e9e31548faa9cbe1f0d
songlw <songlw@yunwutech.com>
2019-05-30 16:47:05 +0800

    文件上传工具

---
+++ src/main/java/com/song/sweetgirl/service/utils/ImageUtils.java
@@ -0,0 +1,76 @@
+/**
+ * 示例说明
+ * <p>
+ * HelloOSS是OSS Java SDK的示例程序，您可以修改endpoint、accessKeyId、accessKeySecret、bucketName后直接运行。
+ * 运行方法请参考README。
+ * <p>
+ * 本示例中的并不包括OSS Java SDK的所有功能，详细功能及使用方法，请参看“SDK手册 > Java-SDK”，
+ * 链接地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/preface.html?spm=5176.docoss/sdk/java-sdk/。
+ * <p>
+ * 调用OSS Java SDK的方法时，抛出异常表示有错误发生；没有抛出异常表示成功执行。
+ * 当错误发生时，OSS Java SDK的方法会抛出异常，异常中包括错误码、错误信息，详细请参看“SDK手册 > Java-SDK > 异常处理”，
+ * 链接地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/exception.html?spm=5176.docoss/api-reference/error-response。
+ * <p>
+ * OSS控制台可以直观的看到您调用OSS Java SDK的结果，OSS控制台地址是：https://oss.console.aliyun.com/index#/。
+ * OSS控制台使用方法请参看文档中心的“控制台用户指南”， 指南的来链接地址是：https://help.aliyun.com/document_detail/oss/getting-started/get-started.html?spm=5176.docoss/user_guide。
+ * <p>
+ * OSS的文档中心地址是：https://help.aliyun.com/document_detail/oss/user_guide/overview.html。
+ * OSS Java SDK的文档地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/install.html?spm=5176.docoss/sdk/java-sdk。
+ */
+
+package com.song.sweetgirl.service.utils;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.ObjectMetadata;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.net.URL;
+
+@Component
+public class ImageUtils {
+
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(ImageUtils.class);
+
+    // endpoint是访问OSS的域名。如果您已经在OSS的控制台上 创建了Bucket，请在控制台上查看域名。
+    // 如果您还没有创建Bucket，endpoint选择请参看文档中心的“开发人员指南 > 基本概念 > 访问域名”，
+    // 链接地址是：https://help.aliyun.com/document_detail/oss/user_guide/oss_concept/endpoint.html?spm=5176.docoss/user_guide/endpoint_region
+    // endpoint的格式形如“http://oss-cn-hangzhou.aliyuncs.com/”，注意http://后不带bucket名称，
+    // 比如“http://bucket-name.oss-cn-hangzhou.aliyuncs.com”，是错误的endpoint，请去掉其中的“bucket-name”。
+    private static String endpoint = "www.liwen.online";
+
+    // accessKeyId和accessKeySecret是OSS的访问密钥，您可以在控制台上创建和查看，
+    // 创建和查看访问密钥的链接地址是：https://ak-console.aliyun.com/#/。
+    // 注意：accessKeyId和accessKeySecret前后都没有空格，从控制台复制时请检查并去除多余的空格。
+    private static String accessKeyId = "LTAIjA8JwE74FmhQ";
+    private static String accessKeySecret = "fOrZYcMOvyu0h9rBEE5IbeI550KKOB";
+
+    // Bucket用来管理所存储Object的存储空间，详细描述请参看“开发人员指南 > 基本概念 > OSS基本概念介绍”。
+    // Bucket命名规范如下：只能包括小写字母，数字和短横线（-），必须以小写字母或者数字开头，长度必须在3-63字节之间。
+    private static String bucketName = "liwen-bucket";
+
+    OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+
+    /**
+     * 图片上传
+     * @param contentType
+     * @param content
+     * @return
+     */
+    public String upload(String contentType, byte[] content) {
+        try {
+            ObjectMetadata meta = new ObjectMetadata();
+            meta.setContentType(contentType);
+            String key = RandomStringUtils.random(20, true, true);
+            this.ossClient.putObject(bucketName, key, new ByteArrayInputStream(content), meta);
+            URL url = new URL("http", endpoint, "/" + key);
+            return url.toString();
+        } catch (Exception e) {
+            this.logger.error("upload error", e);
+            return null;
+        }
+    }
+
+}
