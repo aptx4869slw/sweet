f77cad60da41b4c8f7eb3ea573f00dedcd96c26a
songlw <songlw@yunwutech.com>
2019-05-30 17:54:41 +0800

    图片上传

--- src/main/java/com/song/sweetgirl/service/utils/ImageUtils.java
+++ src/main/java/com/song/sweetgirl/service/utils/ImageUtils.java
@@ -1,23 +1,3 @@
-/**
- * 示例说明
- * <p>
- * HelloOSS是OSS Java SDK的示例程序，您可以修改endpoint、accessKeyId、accessKeySecret、bucketName后直接运行。
- * 运行方法请参考README。
- * <p>
- * 本示例中的并不包括OSS Java SDK的所有功能，详细功能及使用方法，请参看“SDK手册 > Java-SDK”，
- * 链接地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/preface.html?spm=5176.docoss/sdk/java-sdk/。
- * <p>
- * 调用OSS Java SDK的方法时，抛出异常表示有错误发生；没有抛出异常表示成功执行。
- * 当错误发生时，OSS Java SDK的方法会抛出异常，异常中包括错误码、错误信息，详细请参看“SDK手册 > Java-SDK > 异常处理”，
- * 链接地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/exception.html?spm=5176.docoss/api-reference/error-response。
- * <p>
- * OSS控制台可以直观的看到您调用OSS Java SDK的结果，OSS控制台地址是：https://oss.console.aliyun.com/index#/。
- * OSS控制台使用方法请参看文档中心的“控制台用户指南”， 指南的来链接地址是：https://help.aliyun.com/document_detail/oss/getting-started/get-started.html?spm=5176.docoss/user_guide。
- * <p>
- * OSS的文档中心地址是：https://help.aliyun.com/document_detail/oss/user_guide/overview.html。
- * OSS Java SDK的文档地址是：https://help.aliyun.com/document_detail/oss/sdk/java-sdk/install.html?spm=5176.docoss/sdk/java-sdk。
- */
-
 package com.song.sweetgirl.service.utils;
 
 import com.aliyun.oss.OSSClient;
@@ -56,6 +36,7 @@ public class ImageUtils {
 
     /**
      * 图片上传
+     *
      * @param contentType
      * @param content
      * @return
