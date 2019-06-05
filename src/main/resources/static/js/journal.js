b09667fa247ec52d495ff214d7ce0bb2e5569fbe
songlw <songlw@yunwutech.com>
2019-06-05 10:28:01 +0800

    日志数据列表展示

--- src/main/resources/static/js/journal.js
+++ src/main/resources/static/js/journal.js
@@ -1,5 +1,7 @@
 window.onload = initData();
 
+var defaultOffset = '50%';
+
 function initData() {
     $.ajax({
         type: 'GET',
@@ -7,25 +9,21 @@ function initData() {
         url: "api/journals",
         success: function (res) {
             for (i = 0; i < res.length; i++) {
-                var timeline__item = "<div class=\"timeline__item timeline__item--0\">";
-                var timeline__item__station = "<div class=\"timeline__item__station\"></div>";
-                var timeline__item__content = "<div class=\"timeline__item__content\">";
-                var timeline__item__content__date = "<h2 class=\"timeline__item__content__date\">2019 May. 30</h2>";
-                var timeline__item__content__description = "<p class=\"timeline__item__content__description\">所有页面基本完成，准备开始改造，放到后台数据库中去.</p>";
-                var foot = "</div></div>";
-                var journal = timeline__item + timeline__item__station + timeline__item__content + timeline__item__content__date + timeline__item__content__description + foot;
-                $("#journal").prepend(journal);
+                var item0 = "<div class=\"timeline__item timeline__item--" + i + "\">";
+                var item1 = "<div class=\"timeline__item__station\"></div>";
+                var item2 = "<div class=\"timeline__item__content\">";
+                var item3 = "<h2 class=\"timeline__item__content__date\">" + res[i].contentDate + "</h2>";
+                var item4 = "<p class=\"timeline__item__content__description\">" + res[i].content + "</p>";
+                var item5 = "</div></div>";
+                var journal = item0 + item1 + item2 + item3 + item4 + item5;
+                $("#journal").append(journal);
+                customWayPoint('.timeline__item--' + i, 'timeline__item-bg', defaultOffset);
             }
         },
         error: function () {
             alert("发生错误");
         }
     });
-}
-
-var defaultOffset = '50%';
-for (i = 0; i < 17; i++) {
-    customWayPoint('.timeline__item--' + i, 'timeline__item-bg', defaultOffset);
 }
 
 function customWayPoint(className, addClassName, customOffset) {
