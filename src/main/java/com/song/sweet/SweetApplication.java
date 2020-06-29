fb454101049a00a24a260757a559d0af3e613deb
songliwen <songliwen@xiaochirobot.com>
2020-06-29 18:24:24 +0800

    JVM获取时间与本地时间差8小时修复

--- src/main/java/com/song/sweet/SweetApplication.java
+++ src/main/java/com/song/sweet/SweetApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 //@ServletComponentScan
 //@EnableFeignClients
 @EnableCaching
@@ -14,8 +17,13 @@ import org.springframework.cache.annotation.EnableCaching;
 @MapperScan("com.song.sweet.dao")
 public class SweetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SweetApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SweetApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+    }
 
 }
