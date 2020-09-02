7d03234037109d39f972fefb860edc78c2543028
slw <song.liwen@innovation-link.com>
2020-09-02 17:21:13 +0800

    升级swagger版本优化页面样式和配置

--- src/main/java/com/song/sweet/core/config/SwaggerConfig.java
+++ src/main/java/com/song/sweet/core/config/SwaggerConfig.java
@@ -6,8 +6,10 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -23,17 +25,38 @@ public class SwaggerConfig {
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .groupName("Restful APIs")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(SWAGGER_SCAN_BASE_PACKAGE))
                 .paths(PathSelectors.any()) // 可以根据url路径设置哪些请求加入文档，忽略哪些请求
                 .build();
     }
 
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("哩問说")
+                .title("哩問说 Restful APIs")
                 .version(VERSION)
-                .contact("Liwen's")
+                .contact(new Contact("liwen", "#", "liwen@innovation-link.com"))
                 .description("哩問说 博客中所用到的所有API接口的详细文档。")
                 .termsOfServiceUrl("http://www.liwen.cloud")
                 .build();
