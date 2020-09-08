766f8ddafdaf0aadc47d6464f43736ab6bbb36ca
slw <song.liwen@innovation-link.com>
2020-09-08 13:54:56 +0800

    过滤器报错问题修复

--- src/main/java/com/song/sweet/core/ApiFilter.java
+++ src/main/java/com/song/sweet/core/ApiFilter.java
@@ -57,8 +57,6 @@ public class ApiFilter implements Filter {
                 "\"}");
 
         response.setContentType("application/json;charset=UTF-8");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         // 允许跨域
         response.setHeader("Access-Control-Allow-Origin", "*");
         // 允许自定义请求头token(允许head跨域)
@@ -66,33 +64,32 @@ public class ApiFilter implements Filter {
         // 允许前端拿到的header
         response.setHeader("Access-Control-Expose-Headers", "token, Accept, Origin, X-Requested-With, Content-Type, Last-Modified");
 
-        try {
-            Boolean invalid = Boolean.FALSE;
-            String token = request.getHeader(JWTUtils.TOKEN_HEADER);
-            if (StringUtils.isNotBlank(token) && redisUtils != null) {
-                invalid = redisUtils.hasKey("tokenBlackList:" + token);
+        String token = request.getHeader(JWTUtils.TOKEN_HEADER);
+        Boolean invalid = redisUtils.hasKey("tokenBlackList:" + token);
+        if (StringUtils.isNotBlank(token) && token.startsWith(JWTUtils.TOKEN_PREFIX) && JWTUtils.isExpiration(token) && !invalid) {
+            token = token.substring(JWTUtils.TOKEN_PREFIX.length());
+            if (JWTUtils.canRefresh(token)) {
+                redisUtils.setCache("tokenBlackList:" + token, 1, JWTUtils.getExpireTime(token) / 1000);
+                token = JWTUtils.refreshToken(token, false);
             }
-            if (StringUtils.isNotBlank(token) && token.startsWith(JWTUtils.TOKEN_PREFIX) && JWTUtils.isExpiration(token) && !invalid) {
-                token = token.substring(JWTUtils.TOKEN_PREFIX.length());
-                if (JWTUtils.canRefresh(token) && redisUtils != null) {
-                    redisUtils.setCache("tokenBlackList:" + token, 1, JWTUtils.getExpireTime(token) / 1000);
-                    token = JWTUtils.refreshToken(token, Boolean.FALSE);
-                }
-                User user = JWTUtils.getUserInfo(token);
-                if (user != null) {
-                    request.setAttribute("currentUser", user);
-                }
-                response.setStatus(HttpStatus.ACCEPTED.value());
-                request.setAttribute(JWTUtils.TOKEN_HEADER, token);
+            User user = JWTUtils.getUserInfo(token);
+            if (user != null) {
+                request.setAttribute("currentUser", user);
+            }
+            response.setStatus(HttpStatus.ACCEPTED.value());
+            request.setAttribute(JWTUtils.TOKEN_HEADER, token);
 
-                filterChain.doFilter(request, response);
-            } else {
-                filterChain.doFilter(request, response);
-            }
-        } catch (Exception e) {
-            logger.debug(e.getCause().toString());
+            filterChain.doFilter(request, response);
+        } else if (StringUtils.isNotBlank(method) && method.equalsIgnoreCase("OPTIONS")) {
+            response.setHeader("X-Frame-Options", "SAMEORIGIN");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+            response.setHeader("Access-Control-Allow-Headers", "x-requested-with,accept,authorization,content-type");
+            response.setStatus(HttpStatus.ACCEPTED.value());
+        } else {
+            filterChain.doFilter(request, response);
         }
-        filterChain.doFilter(request, response);
     }
 
     @Override
