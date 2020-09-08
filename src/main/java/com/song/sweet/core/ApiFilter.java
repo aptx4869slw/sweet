3d3e27349d42800044fcba68bc665f00b47495ae
slw <song.liwen@innovation-link.com>
2020-09-08 14:22:02 +0800

    过滤器报错问题修复

--- src/main/java/com/song/sweet/core/ApiFilter.java
+++ src/main/java/com/song/sweet/core/ApiFilter.java
@@ -30,8 +30,11 @@ public class ApiFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(ApiFilter.class);
 
-    @Autowired
-    private RedisUtils redisUtils;
+    private final RedisUtils redisUtils;
+
+    public ApiFilter(RedisUtils redisUtils) {
+        this.redisUtils = redisUtils;
+    }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -64,31 +67,38 @@ public class ApiFilter implements Filter {
         // 允许前端拿到的header
         response.setHeader("Access-Control-Expose-Headers", "token, Accept, Origin, X-Requested-With, Content-Type, Last-Modified");
 
-        String token = request.getHeader(JWTUtils.TOKEN_HEADER);
-        Boolean invalid = redisUtils.hasKey("tokenBlackList:" + token);
-        if (StringUtils.isNotBlank(token) && token.startsWith(JWTUtils.TOKEN_PREFIX) && JWTUtils.isExpiration(token) && !invalid) {
-            token = token.substring(JWTUtils.TOKEN_PREFIX.length());
-            if (JWTUtils.canRefresh(token)) {
-                redisUtils.setCache("tokenBlackList:" + token, 1, JWTUtils.getExpireTime(token) / 1000);
-                token = JWTUtils.refreshToken(token, false);
+        try {
+            Boolean invalid = Boolean.FALSE;
+            String token = request.getHeader(JWTUtils.TOKEN_HEADER);
+            if (StringUtils.isNotBlank(token) && redisUtils != null) {
+                invalid = redisUtils.hasKey("tokenBlackList:" + token);
             }
-            User user = JWTUtils.getUserInfo(token);
-            if (user != null) {
-                request.setAttribute("currentUser", user);
-            }
-            response.setStatus(HttpStatus.ACCEPTED.value());
-            request.setAttribute(JWTUtils.TOKEN_HEADER, token);
+            if (StringUtils.isNotBlank(token) && token.startsWith(JWTUtils.TOKEN_PREFIX) && JWTUtils.isExpiration(token) && !invalid) {
+                token = token.substring(JWTUtils.TOKEN_PREFIX.length());
+                if (JWTUtils.canRefresh(token) && redisUtils != null) {
+                    redisUtils.setCache("tokenBlackList:" + token, 1, JWTUtils.getExpireTime(token) / 1000);
+                    token = JWTUtils.refreshToken(token, Boolean.FALSE);
+                }
+                User user = JWTUtils.getUserInfo(token);
+                if (user != null) {
+                    request.setAttribute("currentUser", user);
+                }
+                response.setStatus(HttpStatus.ACCEPTED.value());
+                request.setAttribute(JWTUtils.TOKEN_HEADER, token);
 
-            filterChain.doFilter(request, response);
-        } else if (StringUtils.isNotBlank(method) && method.equalsIgnoreCase("OPTIONS")) {
-            response.setHeader("X-Frame-Options", "SAMEORIGIN");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-            response.setHeader("Access-Control-Allow-Headers", "x-requested-with,accept,authorization,content-type");
-            response.setStatus(HttpStatus.ACCEPTED.value());
-        } else {
-            filterChain.doFilter(request, response);
+                filterChain.doFilter(request, response);
+            } else if (StringUtils.isNotBlank(method) && method.equalsIgnoreCase("OPTIONS")) {
+                response.setHeader("X-Frame-Options", "SAMEORIGIN");
+                response.setHeader("Access-Control-Max-Age", "3600");
+                response.setHeader("Access-Control-Allow-Origin", "*");
+                response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+                response.setHeader("Access-Control-Allow-Headers", "x-requested-with,accept,authorization,content-type");
+                response.setStatus(HttpStatus.ACCEPTED.value());
+            } else {
+                filterChain.doFilter(request, response);
+            }
+        } catch (Exception e) {
+            logger.debug(e.getCause().toString());
         }
     }
 
